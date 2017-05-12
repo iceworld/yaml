8833495f756e869c645e203a27ef8169ce373997
iceworld <gosoumei@hotmail.com>
2017-05-12 17:35:39 +0800

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -4,12 +4,15 @@ netctl netprofile create -b 1000kbps -dscp 1 dev-net-profile-2
 
 #isolation policy, should not impact bandwidth policy here
 netctl policy create allow-diags
-netctl policy rule-add allow-diags 1 -direction=in -protocol=icmp -action=allow
-netctl policy rule-add allow-diags 2 -direction=in -protocol=tcp -port=5201 -action=allow -priority 10
-netctl policy rule-add allow-diags 3 -direction=out -protocol=tcp -port=5201 -action=allow -priority 9
-netctl policy rule-add allow-diags 4 -direction=in -protocol=tcp -action=allow -priority 1
-netctl policy rule-add allow-diags 5 -direction=out -protocol=tcp -action=allow -priority 1
-netctl policy rule-add allow-diags 6 -direction=in -protocol=tcp -port=5900 -action=allow -priority 1
+#deny https outbound traffic using port 443
+netctl policy rule-add allow-diags 1 -direction=out -protocol=tcp -port = 443 -action=deny -priority 1
+#enable vnc, icmp 
+netctl policy rule-add allow-diags 2 -direction=in -protocol=icmp -action=allow
+netctl policy rule-add allow-diags 3 -direction=in -protocol=tcp -port=5201 -action=allow -priority 10
+netctl policy rule-add allow-diags 4 -direction=out -protocol=tcp -port=5201 -action=allow -priority 9
+netctl policy rule-add allow-diags 5 -direction=in -protocol=tcp -action=allow -priority 1
+netctl policy rule-add allow-diags 6 -direction=out -protocol=tcp -action=allow -priority 1
+netctl policy rule-add allow-diags 7 -direction=in -protocol=tcp -port=5900 -action=allow -priority 1
 
 #our chrome pod will use contiv-network and dev-net-profile-2 profile
 netctl group create contiv-net dev-web-group4 -policy=allow-diags -networkprofile=dev-net-profile-2
@@ -19,6 +22,9 @@ netctl group create contiv-net dev-web-group4 -policy=allow-diags -networkprofil
 
 #start chrome pod
 kubectl create -f ./selenium-node-chrome-rc.yaml
+
+#enable https traffic
+netctl policy rule-add allow-diags 1 -direction=out -protocol=tcp -port = 443 -action=allow -priority 1
 
 #change bandwidht to 10mbps to check video quality again
 #using contiv UI should be easier here
